d8044eaa8474a665774a558fe355ed5f619afa63
Justin Wong <justin.wong235@gmail.com>
2021-05-11 20:49:08 -0700

    start end range sql

--- sql/1285_start_end_range.sql
+++ sql/1285_start_end_range.sql
@@ -1,1 +1,24 @@
--- TODO
+/*
+https://stackoverflow.com/questions/17046204/how-to-find-the-boundaries-of-groups-of-contiguous-sequential-numbers
+*/
+
+WITH cte AS (
+  SELECT
+    log_id - row_number() OVER (
+      ORDER BY
+        log_id
+    ) AS grp,
+    log_id
+  FROM
+    Logs
+)
+SELECT
+  min(log_id) AS from_,
+  max(log_id) AS to_
+FROM
+  cte
+GROUP BY
+  grp
+ORDER BY
+  min(log_id)
+
